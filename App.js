712275358849406b5a6da0cd8d8c8657cc6d7585
SuryaKarmakar <surjokarmakarssj7@gmail.com>
2021-04-19 23:34:10 +0530

    text component

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>This is Normal Text Component</Text>
+
+      <Text 
+        ellipsizeMode={'tail'}
+        numberOfLines={1}
+        style={{width:'50%'}}
+        >This is ellipsizeMode text learn more about ellipsizeMode</Text>
+
+      <Text
+        onPress={() => alert('onPress')}>Text Component onPress Listener</Text>
+
+      <Text
+        selectable={true}>You can select this text</Text>
+      
+      <Text 
+        style={{color:'tomato'}}>Text Component can be <Text style={{fontSize:20, fontWeight:'bold'}}>Nested</Text>
+      </Text>
+      
     </View>
   );
 }
---
+++ README.md
@@ -0,0 +1,14 @@
+# React Native Text Component
+
+## Usage
+```
+ $ git clone https://github.com/SuryaKarmakar/React-Native-Text.git
+ $ cd React-Native-Text
+ $ npm install 
+ $ npm run [web, android, ios]
+```
+## Run with Expo 
+```
+ $ expo start --[web, android, ios]
+```
+
